a96e73726e253f16c7db5d66323a61e377a7b5f8
Laspargus <maxime.speroni@gmail.com>
2019-03-04 14:29:58 +0100

    fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,4 @@ class User < ApplicationRecord
 
 
 
-
-
-
-
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_03_04_110856) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
@@ -31,7 +28,6 @@ ActiveRecord::Schema.define(version: 2019_03_04_110856) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,7 +40,5 @@ ActiveRecord::Schema.define(version: 2019_03_04_110856) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "profiles", "users"
-
 end
