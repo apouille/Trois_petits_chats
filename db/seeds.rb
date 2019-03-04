50b388aed7c3bcf320806cc0d32069172ebe7d57
vdepetigny <vdepetigny@yahoo.fr>
2019-03-04 13:23:04 +0100

    [Finish] Ma table items est terminée. Il reste juste les tests models à faire.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'faker'
 
-10 times do
-  item = Item.create!(title: "", description: "", price: , image_url: "")
+10.times do
+   item = Item.create!(title: Faker::Creature::Cat.breed, description: Faker::Lorem.paragraph, price: Faker::Number.decimal(2, 2), image_url: "https://pasteboard.co/I3R1Cuk.jpg")
 end
