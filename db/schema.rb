94e58cbaef67b50c016887c7e78bfeba8131abb8
vdepetigny <vdepetigny@yahoo.fr>
2019-03-04 12:33:26 +0100

    [Progress] Ma table items est en cours de développement. Il me reste à migrer faire le seed et les tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_03_04_105341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.string "author"
+    t.string "format"
+    t.integer "view"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
