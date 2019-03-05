806eb6f33ffaaa4794084d28edd98499195ee36e
vdepetigny <vdepetigny@yahoo.fr>
2019-03-05 16:11:44 +0100

    Clean code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,6 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    @current = current_user.carts.where("status = 0") != nil
-    puts "$"*70
-    puts @current
-    puts "$"*70
   end
 
   def show
