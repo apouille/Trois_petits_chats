9f3660bcb2753b765fd2d3c97863e53be4a57e05
apouille <nguon.melanie@gmail.com>
2019-03-05 12:05:58 +0100

    [finish - add routes and controller on cart and cart_items]

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :items
+  resources :carts
+  resources :cart_items
 end
