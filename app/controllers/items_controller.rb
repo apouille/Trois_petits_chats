746b7131257125dd6831cbbd2d294847f158e8d3
Clodiap <clodiapp@gmail.com>
2019-03-05 12:52:33 +0100

    [fix] jquery added to gemfile + items controller correction

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,7 @@
 class ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all
-    @current_profile = Profile.find_by(user_id: current_user.id)
-    puts "$"*100
-    puts params 
-    puts "$"*100
-    puts Profile.find_by(user_id: current_user.id)
-    puts "$"*100
   end
 
   def show
