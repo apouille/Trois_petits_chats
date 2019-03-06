2f7487ca342a07fcb6c18c291c5de6d6ebd94c9e
Laspargus <maxime.speroni@gmail.com>
2019-03-06 10:59:22 +0100

    fix product show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+##before_action :cart_verification, only: [:create]
 
   def index
     @items = Item.all
@@ -6,5 +7,15 @@ class ItemsController < ApplicationController
 
   def show
   	@item = Item.find(params[:id])
+  end
+
+  private
+#/ Cas où il a des carts mais aucune d'ouvert
+
+  def cart_verification
+    if  current_user.carts.find_by("status = 0") == nil
+      @cart = Cart.new(user_id: current_user.id, status: 0)
+      @cart.save      
+    end
   end
 end
