9b0a5627f2386e664821610de32319419019aa73
vdepetigny <vdepetigny@yahoo.fr>
2019-03-06 19:01:24 +0100

    [Update] Essai mailer

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,29 +1,35 @@
 class ChargesController < ApplicationController
-	def new
-		@cart = current_cart
-	end
+	after_action :confirmation_send, only: [:create]
+		def new
+			@cart = current_cart
+		end
 
-	def create
-		@cart = current_cart
+		def create
+			@cart = current_cart
 
-	  customer = Stripe::Customer.create({
-	    email: params[:stripeEmail],
-	    source: params[:stripeToken],
-	  })
+		  customer = Stripe::Customer.create({
+		    email: params[:stripeEmail],
+		    source: params[:stripeToken],
+		  })
 
-	  charge = Stripe::Charge.create({
-	    customer: customer.id,
-	    amount: @amount,
-	    description: 'Rails Stripe customer',
-	    currency: 'usd',
-	  })
+		  charge = Stripe::Charge.create({
+		    customer: customer.id,
+		    amount: @amount,
+		    description: 'Rails Stripe customer',
+		    currency: 'usd',
+		  })
 
-	  @cart.update(stripe_customer_id: charge[:customer], status: 1)
-	  flash[:notice] = "Your order is complete"
-	  redirect_to root_path
+		  @cart.update(stripe_customer_id: charge[:customer], status: 1)
+		  flash[:notice] = "Your order is complete"
+		  redirect_to root_path
 
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to new_charge_path
-	end
+		rescue Stripe::CardError => e
+		  flash[:error] = e.message
+		  redirect_to new_charge_path
+		end
+
+		def confirmation_send
+      UserMailer.confirmation_email(self).deliver_now
+    end
+
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,16 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
   
+
+  def confirmation_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'troispetitschats-prod.herokuapp.com/users/sign_in' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Confirmation de commande') 
+  end
+  
 end
