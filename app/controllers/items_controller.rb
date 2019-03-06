3e0c1d0be373abe80855212f4a81602e82abf726
Clodiap <clodiapp@gmail.com>
2019-03-06 17:08:54 +0100

    [progress] aws working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,13 +9,36 @@ class ItemsController < ApplicationController
   	@item = Item.find(params[:id])
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(title: params[:title],
+                       description: params[:description],
+                       price: params[:price],
+                       author: params[:author],
+                       view: params[:view],
+                       dimension: params[:dimension],
+                       image_url: params[:image_url]
+                       )
+    @item.item_picture.attach(params[:item_picture])
+
+    if @item.save # essaie de sauvegarder en base @item
+      redirect_to item_path(@item.id), :success => "Item créé !"
+    else
+      render 'new'
+    end
+
+  end
+
   private
 #/ Cas où il a des carts mais aucune d'ouvert
 
   def cart_verification
     if  current_user.carts.find_by("status = 0") == nil
       @cart = Cart.new(user_id: current_user.id, status: 0)
-      @cart.save      
+      @cart.save
     end
   end
 end
