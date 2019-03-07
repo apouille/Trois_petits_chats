f2eaa7aa43a6d6868dbe609f89adf08c480c10a5
Clodiap <clodiapp@gmail.com>
2019-03-07 11:40:58 +0100

    [fix] changé adresses prod (enlevé -prod)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@troispetitschats-prod.com'
+  default from: 'no-reply@troispetitschats.com'
 
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'troispetitschats-prod.herokuapp.com/users/sign_in'
+    @url  = 'troispetitschats.herokuapp.com/users/sign_in'
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
@@ -20,7 +20,7 @@ class UserMailer < ApplicationMailer
     # image à insérer dans l'email si l'url ne fonctionne plus demain matin : <%= image_tag i.item.item_picture, alt: 'item picture' %>
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'troispetitschats-prod.herokuapp.com/users/sign_in'
+    @url  = 'troispetitschats.herokuapp.com/users/sign_in'
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Confirmation de commande')
