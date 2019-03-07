22bfeb1760447c76538e4527d91b0782d614f4c8
Clodiap <clodiapp@gmail.com>
2019-03-07 20:59:25 +0100

    changé le seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,67 +1,19 @@
-2.times do
+1.times do
 	user = User.create!(
-		email: "#{Faker::Name.first_name}@yopmail.com",
-		password: "test1234"
+		email: "troispetitschats@yopmail.com",
+		password: "test1234",
+    is_admin: true
 	)
-end
-1.times do
-  user = User.create!(
-    email: "#{Faker::Name.first_name}@yopmail.com",
-    password: "test1234",
-    is_admin: true,
-  )
-end
-
-20.times do
-   item = Item.create!(
-   	title: Faker::Creature::Cat.breed,
-   	author: Faker::Name.name,
-   	dimension: "300x500",
-   	price: Faker::Number.decimal(2, 2),
-   	image_url: "http://placekitten.com/500/300",
-  	description: Faker::Lorem.characters(41),
-    view: rand(0..10)
-  )
-end
-
-
-1.times do
- 	profile = Profile.create!(
- 		first_name: Faker::Name.first_name,
- 		last_name: Faker::Name.last_name,
- 		street: Faker::Address.street_address,
- 		city: Faker::Address.city,
- 		zip_code: "75000",
- 		phone_number: "0033#{rand(100000000..999999999)}",
- 		user: User.first
- 	)
 end
 
 1.times do
   profile = Profile.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    street: Faker::Address.street_address,
-    city: Faker::Address.city,
+    first_name: 'John',
+    last_name: 'Doe',
+    street: '19 rue de THP',
+    city: 'THP-City',
     zip_code: "75000",
-    phone_number: "0033#{rand(100000000..999999999)}",
-    user: User.last
+    phone_number: "0607080910",
+    user: User.first
   )
-
-end
-
-1.times do
-  cart = Cart.create!(
-    status: 0,
-    user: User.first,
-    )
-  puts 'cart created'
-end
-
-1.times do
-  cart = Cart.create!(
-    status: 1,
-    user: User.last,
-    )
-  puts 'cart created'
 end
