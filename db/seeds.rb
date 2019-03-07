77e8d7c35415da97b0765689226991637129d546
vdepetigny <vdepetigny@yahoo.fr>
2019-03-07 11:31:09 +0100

    [Fix] ceal seeds (only users & profiles)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,6 @@
 	)
 end
 
-20.times do
-   item = Item.create!(
-   	title: Faker::Creature::Cat.breed,
-   	author: Faker::Name.name,
-   	dimension: "300x500",
-   	price: Faker::Number.decimal(2, 2),
-   	image_url: "http://placekitten.com/500/300",
-  	description: Faker::Lorem.characters(41),
-    view: rand(0..10)
-  )
-end
-
-
 1.times do
  	profile = Profile.create!(
  		first_name: Faker::Name.first_name,
@@ -28,42 +15,4 @@ end
  		phone_number: "0033#{rand(100000000..999999999)}",
  		user: User.all.sample
  	)
-
 end
-
-1.times do
-  cart = Cart.create!(
-    status: 0,
-    user: User.first,
-    )
-  puts 'cart created'
-end
-
-1.times do
-  cart = Cart.create!(
-    status: 1,
-    user: User.first,
-    )
-  puts 'cart created'
-end
-
-
-3.times do
-  cartitems = CartItem.create!(
-    cart: Cart.first,
-    item: Item.all.sample,
-    quantity: 1
-    )
-end
-
-3.times do
-  cartitems = CartItem.create!(
-    cart: Cart.last,
-    item: Item.all.sample,
-    quantity: 1
-    )
-end
-
-
-
-
