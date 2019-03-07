cf152a270d34eaff115377d850d427e839b0e7d3
Laspargus <maxime.speroni@gmail.com>
2019-03-07 15:30:00 +0100

    add admin user show

--- app/controllers/admin/carts_controller.rb
+++ app/controllers/admin/carts_controller.rb
@@ -4,4 +4,9 @@ class Admin::CartsController < AdminController
 	@orders = Cart.all.where("status=1")
 	@carts = Cart.all.where("status=0")
 	end
+
+	def show
+	@cart = Cart.find(params[:id])
+	@cart_items = @cart.cart_items
+	end
 end
